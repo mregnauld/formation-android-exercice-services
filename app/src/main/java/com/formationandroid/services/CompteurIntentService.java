0e8734ae21948c49e33b082a97f53a5c6f1cd170
mregnauld <mregnauld@gmail.com>
2020-01-13 15:58:29 +0100

    Mise a jour Android 10

--- app/src/main/java/com/formationandroid/services/CompteurIntentService.java
+++ app/src/main/java/com/formationandroid/services/CompteurIntentService.java
@@ -2,8 +2,9 @@ package com.formationandroid.services;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.support.annotation.Nullable;
 import android.util.Log;
+
+import androidx.annotation.Nullable;
 
 
 public class CompteurIntentService extends IntentService
--- app/src/main/java/com/formationandroid/services/DetailActivity.java
+++ app/src/main/java/com/formationandroid/services/DetailActivity.java
@@ -6,9 +6,10 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity
 {
